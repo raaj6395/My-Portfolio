9afe70818afabe8218f34dd460517a27ac0d69b1
ANKIT RAJ <111572476+raaj6395@users.noreply.github.com>
2025-12-03 00:27:27 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = ()=>{
+  return(
+    <div className="container mx-auto max-w-7xl">
+      Hello World
+      {/* here we are going to add all the components for the portfolio */}
+    </div>
+  )
+};
+export default App;
